86135d38864cf15e890c91779f859b6186a84887
Bekzat Sundetkhan <121685401+Bekzat158@users.noreply.github.com>
2024-02-11 21:19:46 +0600

    Update app.js

--- app.js
+++ app.js
@@ -10,11 +10,16 @@ const { verifyToken, redirectToHomeIfLoggedIn, ifAdmin } = require('./utils/auth
 const app = express();
 
 // Connect to MongoDB Atlas using environment variable
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log('Connected to MongoDB Atlas');
-    })
-    .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Set a shorter timeout for server selection
+    socketTimeoutMS: 45000 // Set a longer socket timeout
+})
+.then(() => {
+    console.log('Connected to MongoDB Atlas');
+})
+.catch(err => console.error('MongoDB connection error:', err));
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
