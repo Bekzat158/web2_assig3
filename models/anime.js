c9ed70eacadaadeea5f562f7960b655c36161a86
Bekzat Sundetkhan <121685401+Bekzat158@users.noreply.github.com>
2024-02-12 19:13:23 +0600

    Update anime.js

--- models/anime.js
+++ models/anime.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const animeSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Ссылка на пользователя
+    search: { type: String }, // Add a search field,
     animeList: [{
         imageUrl: { type: String, required: true },
         titleEnglish: String,
@@ -9,9 +10,12 @@ const animeSchema = new mongoose.Schema({
         episodes: Number,
         duration: String,
         genres: [String]
-    }]
+    }],
 }, {
     timestamps: true // Добавление меток времени
 });
+
+// Indexing the search field for faster search queries
+animeSchema.index({ search: 'text' });
 
 module.exports = mongoose.model('Anime', animeSchema);
